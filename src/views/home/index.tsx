86225e0089bfd6e9d7caad9cd877a1f01398cd40
fathrezza146 <fathrezzafirmanull@refactory.id>
2023-08-15 11:05:03 +0700

    fix tailwind not working on vite

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="text-red-500">Test BG
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <h1 className="">
-      Hello world!
-    </h1>
+  return (
+    <div className="bg-red-500">
+      Test BG
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="">Hello world!</h1>
     </div>
-}
+  );
+};
 
-export default Home
+export default Home;
