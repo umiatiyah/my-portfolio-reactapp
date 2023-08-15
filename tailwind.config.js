06d2fd8040102a3405f4e302a42a388e2047b8ae
fathrezza146 <fathrezzafirmanull@refactory.id>
2023-08-15 11:05:03 +0700

    fix tailwind not working on vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
-
-// eslint-disable-next-line no-undef
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
