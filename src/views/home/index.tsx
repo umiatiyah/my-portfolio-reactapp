9eed4fd9a920d2208b82fe600ac106492b883254
umiatiyah <umiatiyah97@gmail.com>
2023-08-17 00:42:35 +0700

    replace navbar, header and footer to routes

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,22 +1,14 @@
 import React from "react";
-import Header from "~/layouts/header";
 import Contact from "./components/contact";
-import Footer from "~/layouts/footer";
-import Navbar from "~/layouts/navbar";
 import About from "./components/about";
 import Portfolio from "./components/portfolio";
 
 const Home = () => {
   return (
     <>
-      <main className="bg-gray-900 text-white font-sans">
-        <Navbar />
-        <Header />
-        <About />
-        <Portfolio />
-        <Contact />
-        <Footer />
-      </main>
+      <About />
+      <Portfolio />
+      <Contact />
     </>
   );
 };
