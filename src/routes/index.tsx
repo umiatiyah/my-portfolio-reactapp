7f3930b2e182205238f49bf97916c6997c5e8b66
umiatiyah <umiatiyah97@gmail.com>
2023-08-17 00:42:35 +0700

    replace navbar, header and footer to routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,23 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../views";
-
+import Navbar from "~/layouts/navbar";
+import Header from "~/layouts/header";
+import Footer from "~/layouts/footer";
 
 const Navigations = () => {
-
-    return (    
-    <BrowserRouter >
+  return (
+    <BrowserRouter>
+      <main className="bg-gray-900 text-white font-sans">
+        <Navbar />
+        <Header />
         <Routes>
-           <Route path="" element ={<Home />}  />
+          <Route path="" element={<Home />} />
         </Routes>
+        <Footer />
+      </main>
     </BrowserRouter>
-   )
-}
+  );
+};
 
-export default Navigations
+export default Navigations;
