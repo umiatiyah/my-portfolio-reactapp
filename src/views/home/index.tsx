0e55f98dbc7a70177200d7686f985d7f3263d939
umiatiyah <umiatiyah97@gmail.com>
2023-08-17 22:21:05 +0700

    replace each section in home index to each folder

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Contact from "./components/contact";
-import About from "./components/about";
-import Portfolio from "./components/portfolio";
+import Contact from "./contact/contact";
+import About from "./about/about";
+import Portfolio from "./portfolio/portfolio";
 
 const Home = () => {
   return (
