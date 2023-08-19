4737330bddc447e24d2620bfcdc19ad24c6e7416
umiatiyah <umiatiyah97@gmail.com>
2023-08-19 21:30:12 +0700

    refactor components folder

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../views";
-import Footer from "~/components/footer";
-import Navbar from "~/components/navbar";
-import Header from "~/components/header";
+import { Footer, Header, Navbar } from "~/components";
 
 const Navigations = () => {
   return (
---
+++ src/views/home/components/index.ts
@@ -0,0 +1,5 @@
+import About from "./about";
+import Contact from "./contact";
+import Portfolio from "./portfolio";
+
+export { About, Contact, Portfolio };
