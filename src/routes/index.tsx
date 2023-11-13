941c0975a7d5cf52235feb2533bb25be0faf14fc
umiatiyah <umiatiyah97@gmail.com>
2023-11-14 01:53:05 +0700

    integration user profile in home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../views";
 import { Footer, Header, Navbar } from "~/components";
+import { UseHome } from "~/views/home/hooks";
 
 const Navigations = () => {
+  const data = UseHome();  
   return (
     <BrowserRouter>
       <main className="bg-gray-900 text-white font-sans">
         <Navbar />
-        <Header />
+        <Header user_profile={data.user.user_profile} />
         <Routes>
           <Route path="" element={<Home />} />
         </Routes>
